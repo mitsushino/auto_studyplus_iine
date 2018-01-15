33cc8e6900c8780dbb0ca89141bace3ae9272fb5
mitsushino <rkccckbk3@gmail.com>
2018-01-15 17:05:21 +0900

    Login from top page

--- auto_studyplus_iine.py
+++ auto_studyplus_iine.py
@@ -1,4 +1,10 @@
 # -*- coding: utf-8 -*-
+# todo:exe形式にしてGUIをつけさせる
+# todo:メールアドレスとパスワードは初回起動時空白とし
+#      入力後、正常終了した場合,config.iniを作成して終了
+#      次回起動時は生成したconfig.iniを使う
+# todo:studyplusのトップページからログインさせ、studylogに移動し自動入力させる
+
 
 import configparser
 from selenium import webdriver
@@ -15,16 +21,24 @@ def get_config_info(file_name, group, item):
 def auto_iine():
     """Click Like! buttons that haven't been clicked"""
     ini_file = 'config.ini'
-    url = get_config_info(ini_file, 'URL', 'url')
+    # url = get_config_info(ini_file, 'URL', 'url')
     mail = get_config_info(ini_file, 'LOGIN', 'mail')
     driver = webdriver.Chrome()
-    driver.get(url)
+    driver.get('https://www.studyplus.jp/')
+    login_form = driver.find_element_by_class_name('link-dummy')
+    login_form.click()
+    driver.implicitly_wait(5)
     print('Please Login')
-    username_element = driver.find_element_by_name('username')
+
+    username_element = driver.find_element_by_xpath(
+        '/html/body/bs-modal[1]/div/div/bs-modal-body/div/form/div[1]/div[1]/div/div[2]/input')
     username_element.send_keys(mail)
-    password_element = driver.find_element_by_name('password')
+    password_element = driver.find_element_by_xpath(
+        '/html/body/bs-modal[1]/div/div/bs-modal-body/div/form/div[1]/div[2]/div/div[2]/input')
     password_element.send_keys('')
-    while 'sign_in' in driver.current_url:
+    header_sub_menu_element = driver.find_element_by_id('headSubMenu-list')
+    while 'マイページ' not in header_sub_menu_element.text:
+        header_sub_menu_element = driver.find_element_by_id('headSubMenu-list')
         time.sleep(3)
     print('Login Success')
 
